5c74bd8e8dfb3425ca0a5448ae6299f608143a85
Necii <neciisteve9@gmail.com>
2024-02-16 17:55:40 +0100

    formated code

--- src/Employees.jsx
+++ src/Employees.jsx
@@ -1,26 +1,26 @@
 import Teams from './Teams';
 import TeamMembers from './TeamMembers';
 
-const Employees = ({employees, selectedTeam, handleEmployeeCardClick, handleTeamSelectionChange}) => {
-return (
- <main className="container">
-   <div className="row justify-content-center mt-3 mb-3">
-   <div className="col-8">
-     <Teams selectedTeam={selectedTeam} 
-       handleTeamSelectionChange = {handleTeamSelectionChange} />
-   </div>
-   </div>
-<div class="row justify-content-center mt-3 mb-3">
-<div class="col-8">
-<div class="card-collection">
-<TeamMembers employees = {employees} 
-                  handleEmployeeCardClick = {handleEmployeeCardClick} 
-                  selectedTeam={selectedTeam} />
-</div>  
-</div>
-</div>
- </main>
-)  
+const Employees = ({ employees, selectedTeam, handleEmployeeCardClick, handleTeamSelectionChange }) => {
+  return (
+    <main className="container">
+      <div className="row justify-content-center mt-3 mb-3">
+        <div className="col-8">
+          <Teams selectedTeam={selectedTeam}
+            handleTeamSelectionChange={handleTeamSelectionChange} />
+        </div>
+      </div>
+      <div class="row justify-content-center mt-3 mb-3">
+        <div class="col-8">
+          <div class="card-collection">
+            <TeamMembers employees={employees}
+              handleEmployeeCardClick={handleEmployeeCardClick}
+              selectedTeam={selectedTeam} />
+          </div>
+        </div>
+      </div>
+    </main>
+  )
 
 }
 
